4896464ebaf7187520b95d879d49e8472c42bf63
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2018-02-16 16:36:45 -0600

    add info route

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var users = require('./routes/users');
+var info = require('./routes/info');
 
 var app = express();
 
