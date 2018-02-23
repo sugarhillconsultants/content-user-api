7a52722e3c421088f759b05c8c65c267e9d0bc3e
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2018-02-23 10:14:56 -0600

    parse VCAP

--- routes/info.js
+++ routes/info.js
@@ -6,7 +6,7 @@ var appEnv = cfenv.getAppEnv();
 router.get('/info', function(req, res, next) {
   console.log("info");
   if (!appEnv.isLocal) {
-    res.json(process.env.VCAP_SERVICES);
+    res.json(JSON.parse(process.env.VCAP_SERVICES));
   } else {
     res.json({ "message": "This applications is not using VAC_SERVICES."});
   }
