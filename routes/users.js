3568c6c5f8c6ea225a546e7312c68adc6ae28c67
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2018-02-23 12:39:54 -0600

    add put and post

--- routes/users.js
+++ routes/users.js
@@ -26,7 +26,7 @@ router.get('/users/:user_id', function(req, res, next) {
   });
 })
 
-router.post('/add', function(req, res, next) {
+router.post('/users', function(req, res, next) {
   User.findOne({ username: req.body.username }, function(err, user) {
     if (user) {
       res.json(user);
@@ -34,9 +34,17 @@ router.post('/add', function(req, res, next) {
       var user = new User();
 
       //Set the provider properties that came from the POST data
+      console.log(req.body)
       user.username = req.body.username;
       user.email = req.body.email;
-      user.password = req.body.password;
+      user.first_name = req.body.first_name;
+      user.last_name = req.body.last_name;
+
+      if (req.body.active) {
+        user.active = req.body.active;
+      } else {
+        user.active = true
+      }
 
       user.save(function(err, user_data) {
         if (err) {
@@ -46,6 +54,38 @@ router.post('/add', function(req, res, next) {
 
         res.json(user_data);
       });
+    }
+  });
+});
+
+router.put('/users/:user_id', function(req, res, next) {
+  User.findById(req.params.user_id, function(err, user) {
+    if (err) {
+      res.status(404).send(err);
+    } else if (user) {
+      //Set the provider properties that came from the POST data
+      console.log(req.body)
+      user.username = req.body.username;
+      user.email = req.body.email;
+      user.first_name = req.body.first_name;
+      user.last_name = req.body.last_name;
+
+      if (req.body.active) {
+        user.active = req.body.active;
+      } else {
+        user.active = true
+      }
+
+      user.save(function(err, user_data) {
+        if (err) {
+          console.log(err);
+          res.status(500).send(err);
+        }
+
+        res.json(user_data);
+      });
+    } else {
+      res.status(404).send();
     }
   });
 });
