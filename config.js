44a683fb1694c348e3da56b09629d32ce3a7bbcf
Travis N. Thomsen <travis.n.thomsen@gmail.com>
2018-03-08 13:58:52 -0600

    update nr

--- config.js
+++ config.js
@@ -2,15 +2,13 @@ var cfenv = require("cfenv");
 var appEnv = cfenv.getAppEnv();
 
 var newrelic_license_key, vcaps_services, uri;
+newrelic_license_key = process.env.NEW_REILIC_LICENSE_KEY;
 
 if (appEnv.isLocal) {
   uri = 'mongodb://localhost:27017/users';
-  newrelic_license_key = process.env.NEW_REILIC_LICENSE_KEY;
 } else {
   vcaps_services = JSON.parse(process.env.VCAP_SERVICES);
   uri = vcaps_services['mlab'][0].credentials.uri;
-   newrelic_service = JSON.parse(appEnv.getService("newrelic"));
-   newrelic_license_key = newrelic_service.newrelic_license_key.credentials.license_key;
 }
 
 module.exports = {
--- routes/newrelic.js
+++ routes/newrelic.js
@@ -4,9 +4,15 @@ var cfenv = require("cfenv");
 var appEnv = cfenv.getAppEnv();
 var config = require('./../config');
 
+if (appEnv.isLocal) {
+  newrelic_license_key = process.env.NEW_REILIC_LICENSE_KEY;
+} else {
+  newrelic_service = appEnv.getService("newrelic");
+}
+
 router.get('/newrelic', function(req, res, next) {
   console.log("config");
-  res.json({ "newrelic_license_key": config.newrelic_license_key });
+  res.json(newrelic_service);
 });
 
 module.exports = router;
